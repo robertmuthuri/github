0efe46d09f160618fdf8892847327e2e472981c0
Robert Muthuri <muthuri.r@gmail.com>
2019-08-23 11:37:40 +0300

    Added a root component repo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RepoComponent } from './repo/repo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepoComponent
   ],
   imports: [
     BrowserModule,
