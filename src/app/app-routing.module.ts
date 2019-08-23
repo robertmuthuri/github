8f73cec853f885654356d58c4abf9f558a2a640b
Robert Muthuri <muthuri.r@gmail.com>
2019-08-23 12:39:42 +0300

    Adds basic routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { RepoComponent } from './repo/repo.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: RepoComponent},
+  { pathMatch: 'full', redirectTo: 'home', path: '' },
+  { path: '**', component: NotFoundComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
