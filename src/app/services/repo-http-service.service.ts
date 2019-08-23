69aab141d02a4fab1cd9422bc7fc70ac78459137
Robert Muthuri <muthuri.r@gmail.com>
2019-08-23 17:11:35 +0300

    Import HttpClient Module and make a http request using a promise.

--- src/app/services/repo-http-service.service.ts
+++ src/app/services/repo-http-service.service.ts
@@ -1,13 +1,35 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import {error} from 'util';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepoHttpServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   searchRepos(searchTerm: string) {
     console.log(searchTerm);
+
+    let searchPoint = 'https://api.github.com/users/robertmuthuri?access_token=' + environment.RepoAPIKey;
+
+    searchPoint += searchTerm;
+
+    let promise = new Promise((resolve, reject) => {
+      this.http.get(searchPoint).toPromise().then(
+        (results) => {
+          console.log(results);
+          resolve();
+        },
+        (error) => {
+          console.log(error);
+          reject();
+        }
+      );
+    });
+    return promise;
+
   }
 }
