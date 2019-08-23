a242e55eac954e747b64dfa968214283c3983a8e
Robert Muthuri <muthuri.r@gmail.com>
2019-08-23 12:52:09 +0300

    Adds repo-search component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { RepoComponent } from './repo/repo.component';
 import { RepoNavbarComponent } from './repo-navbar/repo-navbar.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { RepoSearchComponent } from './repo-search/repo-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RepoComponent,
     RepoNavbarComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    RepoSearchComponent
   ],
   imports: [
     BrowserModule,
